24394709bcb33cb32acbfa73ff51e7bdeedcf758
Ian Halbwachs <preferred.nomenclature@gmail.com>
2017-02-13 14:37:54 -0500

    lunch is over

---
+++ browser/react/containers/StationsContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import Stations from '../components/Stations'
+
+function convertSongsToStations (songs) {
+ console.log(this.state)
+}
+
+convertSongsToStations();
+
+function mapStateToProps (state) {
+  return {};
+}
+
+function mapDispatchToProps (dispatch) {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stations);
